24d966c8adeba9f69632bd686d84129eb1015cad
Yachitzu <104957983+Yachitzu@users.noreply.github.com>
2022-05-21 02:32:48 -0500

    Add files via upload
    Proyecto Principal

---
+++ ProyectoCV/src/proyectocv/ProyectoCV.java
@@ -0,0 +1,17 @@
+
+package proyectocv;
+
+/**
+ *
+ * @author Tefy
+ */
+public class ProyectoCV {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
